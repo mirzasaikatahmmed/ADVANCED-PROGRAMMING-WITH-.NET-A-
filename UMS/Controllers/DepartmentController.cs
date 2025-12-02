6addcfb2f92a5074aa2a1043da5734fdd0765ae0
MIRZA SAIKAT AHMMED <mirzasaikatahmmed@gmail.com>
2025-12-02 13:54:42 +0600

    ╬ô├▓┬╝Γö£Γöñ╬ô├╢┬ú╬ô├╗├┤╬ô├╢┬╝╬ô├▓┬ÑΓò¼├┤Γö£ΓòóΓö¼├║Γò¼├┤Γö£ΓòóΓö£ΓûÆ╬ô├▓┬╝Γö£Γöñ╬ô├╢┬ú╬ô├▓├│╬ô├╢┬╝╬ô├▓┬ÑΓò¼├┤Γö£ΓòóΓö¼├║Γò¼├┤Γö£ΓûôΓö£┬¬╬ô├▓┬╝Γö£Γöñ╬ô├╢┬ú╬ô├▓├│╬ô├╢┬╝╬ô├▓┬Ñ╬ô├▓┬╝Γö£Γöñ╬ô├╢┬ú╬ô├▓├│╬ô├╢┬úΓö£┬╜ feat(department): Add CRUD functionality for Departments

---
+++ UMS/Controllers/DepartmentController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UMS.EF;
+using UMS.DTOs;
+
+namespace UMS.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private Mid_Lab_Final_TaskEntities db = new Mid_Lab_Final_TaskEntities();
+
+        // GET: Department
+        public ActionResult Index()
+        {
+            var data = db.Departments
+                         .Select(d => new DepartmentDTO
+                         {
+                             Id = d.Id,
+                             DepartmentName = d.DepartmentName
+                         }).ToList();
+
+            return View(data);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var dept = db.Departments.Find(id);
+            if (dept == null) return HttpNotFound();
+
+            var dto = new DepartmentDTO
+            {
+                Id = dept.Id,
+                DepartmentName = dept.DepartmentName
+            };
+
+            return View(dto);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(DepartmentDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dept = new Department
+                {
+                    DepartmentName = model.DepartmentName
+                };
+
+                db.Departments.Add(dept);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var dept = db.Departments.Find(id);
+            if (dept == null) return HttpNotFound();
+
+            var dto = new DepartmentDTO
+            {
+                Id = dept.Id,
+                DepartmentName = dept.DepartmentName
+            };
+
+            return View(dto);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(DepartmentDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dept = db.Departments.Find(model.Id);
+                if (dept == null) return HttpNotFound();
+
+                dept.DepartmentName = model.DepartmentName;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var dept = db.Departments.Find(id);
+            if (dept == null) return HttpNotFound();
+
+            var dto = new DepartmentDTO
+            {
+                Id = dept.Id,
+                DepartmentName = dept.DepartmentName
+            };
+
+            return View(dto);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var dept = db.Departments.Find(id);
+            if (dept == null) return HttpNotFound();
+
+            db.Departments.Remove(dept);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ UMS/DTOs/DepartmentDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UMS.DTOs
+{
+    public class DepartmentDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string DepartmentName { get; set; }
+    }
+}
